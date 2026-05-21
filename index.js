487e279b7fa56161fa66983fee721b43652b8b69
craydinitzmar-cmyk <craydinitzmar@gmail.com>
2026-05-21 05:47:28 +0300

    Refactor index.js to set up Express server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Client Conversion Bot is running");
+});
+
+app.listen(port, () => {
+  console.log("Server running");
+});
+"scripts": {
+  "start": "node index.js"
+}
